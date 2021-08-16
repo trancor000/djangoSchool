9e9b98cc4668115f17002cabe4fe636f83c4ce1c
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-08-15 19:12:39 -0700

    Update admin.py

--- src/mainapp/classApp/admin.py
+++ src/mainapp/classApp/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import djangoClasses
 from .models import djangoSchool
-from .models import djangoAid
+from .models import djangoAide
 from .models import djangoGraduation
 
 
 admin.site.register(djangoClasses)
 admin.site.register(djangoSchool)
-admin.site.register(djangoAid)
+admin.site.register(djangoAide)
 admin.site.register(djangoGraduation)
